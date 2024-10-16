e761d3eafc0358b4265a3228e7211eb50eb0bd00
jyothi-suraneni <jyothisuraneni426@gmail.com>
2024-10-16 16:34:07 +0530

    Add files via upload

--- GenericPair.java
+++ GenericPair.java
@@ -1,10 +1,47 @@
 package Revature;
 
+class Pair<T,U>{
+	private T first;
+	private U second;
+	public Pair(T first, U second) {
+		super();
+		this.first = first;
+		this.second = second;
+	}
+	public T getFirst() {
+		return first;
+	}
+	public void setFirst(T first) {
+		this.first = first;
+	}
+	public U getSecond() {
+		return second;
+	}
+	public void setSecond(U second) {
+		this.second = second;
+	}
+	@Override
+	public String toString() {
+		return "Pair [first=" + first + ", second=" + second + "]";
+	}
+	public Pair() {
+		super();
+	}
+	
+	
+}
 public class GenericPair {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Pair<Integer,String> pair=new Pair<>(1,"Jyothi");
+		System.out.println("First : "+pair.getFirst());
+		System.out.println("Second : "+pair.getSecond());
+		
+		pair.setFirst(99);
+		pair.setSecond("Hii");
+		System.out.println(pair.toString());
 	}
+
+	
 
 }
